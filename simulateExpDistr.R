172cc2fd9ed0314f2b650c6367a21eace90d10db
lfenster <lfenster@microsoft.com>
2014-09-18 12:54:18 -0400

    initial code for part 1

---
+++ simulateExpDistr.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+simulateExpDistr<-function(nosim, observations, lambda)
+{
+    observationDF<-replicate(nosim, rexp(observations,lambda))
+    
+   # par(mfrow=c(2,2))
+    means <- cumsum(colMeans(observationDF))/(1:nosim)    
+    plot(1:nosim, means, type = "l", lwd = 2, frame = FALSE, ylab = "cumulative means",  xlab = "sample size") 
+    abline(h = 1/lambda)
+    
+    
+    sds <- cumsum(apply(observationDF, 2, sd))/(1:nosim) 
+    plot(1:nosim, sds, type = "l", lwd = 2, frame = FALSE, ylab = "cumulative standard deviations",  xlab = "sample size") 
+    abline(h = 1/lambda)
+    
+    cfunc <- function(x, n) (mean(x) - 1/lambda) / ((1/lambda)/(sqrt(n)))
+     dat <- data.frame(
+          x = c(apply(observationDF, 2, cfunc, observations)),
+          size = factor(c(observations), rep(nosim))
+     )
+    g <- ggplot(dat, aes(x = x)) + geom_histogram(alpha = .20, binwidth=.3, colour = "black", fill="green", aes(y = ..density..)) 
+    g <- g + stat_function(fun = dnorm, size = 2)
+    g
+}
